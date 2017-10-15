fdc378895fc38d31355d882ec44ca34a989b273d
sajan0007 <grzesiek21217@wp.pl>
2017-10-15 19:02:12 +0200

    Made out parameter for linetrace

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -32,9 +32,21 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	//Get world location if linetrace through crosshair
-	//If it hits the landscape
-		//Tell controlled thank to aim at this point
+	FVector HitLocation; //Out parameter
+	if (GetSightRayHitLocation(HitLocation)) //Has "side-effect", is goin to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+
+		//TODO Tell controlled thank to aim at this point
+	}
+}
+
+//Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
+{
+	
+	OutHitLocation = FVector(1.0);
+	return true;
 }
 
 
